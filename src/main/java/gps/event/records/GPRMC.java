4f9bed1e060f15609426a4ab478cac0078ab58de
DennisGuo <guohengxi.dennis@gmail.com>
2019-04-11 08:42:47 +0800

    solve bug when parse speed and bearing

--- src/main/java/gps/event/records/GPRMC.java
+++ src/main/java/gps/event/records/GPRMC.java
@@ -15,79 +15,84 @@ import java.util.TimeZone;
 
 /**
  * Recommended minimum
- * 
  */
 public class GPRMC extends GpsInfoRecord {
 
 
-	private static SimpleDateFormat dateFormat = new SimpleDateFormat(
-			"ddMMyy HHmmss.SSS");
-	static {
-		// set the timezone for analysis of dates on UTC
-		dateFormat
-				.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
-	}
+    private static SimpleDateFormat dateFormat = new SimpleDateFormat(
+            "ddMMyy HHmmss.SSS");
 
-	private boolean isValid;
-	private double latitude;
-	private double longitude;
-	private double speed;
-	private double direction;
-	private Date readingDate;
+    static {
+        // set the timezone for analysis of dates on UTC
+        dateFormat
+                .setCalendar(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
+    }
+
+    private boolean isValid;
+    private double latitude;
+    private double longitude;
+    private double speed;
+    private double direction;
+    private Date readingDate;
 
 
-	protected GPRMC(String[] fields) {
-		super(fields);
+    protected GPRMC(String[] fields) {
+        super(fields);
 
-		//log.debug("GPRMC (Recommended Minimum)");
-		try {
-			this.readingDate = dateFormat.parse(fields[9] + " " + fields[1]);
-		} catch (ParseException e) {
-			//log.error("Couldn't parse reading date", e);
-			//log.debug("Will assume _now_ as reading date");
-			this.readingDate = new Date();
-		}
-		this.isValid = fields[2].equals("A");
-		if (this.isValid) {
+        //log.debug("GPRMC (Recommended Minimum)");
+        try {
+            this.readingDate = dateFormat.parse(fields[9] + " " + fields[1]);
+        } catch (ParseException e) {
+            //log.error("Couldn't parse reading date", e);
+            //log.debug("Will assume _now_ as reading date");
+            this.readingDate = new Date();
+        }
+        this.isValid = fields[2].equals("A");
+        if (this.isValid) {
 
-			String rawLat = fields[3];
-			String rawLong = fields[5];
-			if (rawLat.length() > 0 && rawLong.length() > 0) {
-				// we have a winner
-				int degrees = Integer.parseInt(rawLat.substring(0, 2));
-				double minutes = Double.parseDouble(rawLat.substring(2));
-				this.latitude = degrees + minutes / 60;
-				if (fields[4].equals("S")) {
-					this.latitude *= -1;
-				}
-				degrees = Integer.parseInt(rawLong.substring(0, 3));
-				minutes = Double.parseDouble(rawLong.substring(3));
-				this.longitude = degrees + minutes / 60;
-				if (fields[6].equals("W")) {
-					this.longitude *= -1;
-				}
-			}
-			// speed and direction
-            this.speed = Double.parseDouble(fields[7]) * 1.852; // km/h
-            this.direction = Double.parseDouble(fields[8]); // 0 - 360
-		}
-	}
+            String rawLat = fields[3];
+            String rawLong = fields[5];
+            if (rawLat.length() > 0 && rawLong.length() > 0) {
+                // we have a winner
+                int degrees = Integer.parseInt(rawLat.substring(0, 2));
+                double minutes = Double.parseDouble(rawLat.substring(2));
+                this.latitude = degrees + minutes / 60;
+                if (fields[4].equals("S")) {
+                    this.latitude *= -1;
+                }
+                degrees = Integer.parseInt(rawLong.substring(0, 3));
+                minutes = Double.parseDouble(rawLong.substring(3));
+                this.longitude = degrees + minutes / 60;
+                if (fields[6].equals("W")) {
+                    this.longitude *= -1;
+                }
+            }
+            // speed and direction
+            if (!fields[7].equalsIgnoreCase("")) {
+                this.speed = Double.parseDouble(fields[7]) * 1.852; // km/h
+            }
+            if (!fields[8].equalsIgnoreCase("")) {
 
-	public boolean isValid() {
-		return isValid;
-	}
+                this.direction = Double.parseDouble(fields[8]); // 0 - 360}
+            }
+        }
+    }
 
-	public double getLatitude() {
-		return latitude;
-	}
+    public boolean isValid() {
+        return isValid;
+    }
 
-	public double getLongitude() {
-		return longitude;
-	}
+    public double getLatitude() {
+        return latitude;
+    }
 
-	public Date getReadingDate() {
-		return readingDate;
-	}
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public Date getReadingDate() {
+        return readingDate;
+    }
 
     public double getSpeed() {
         return speed;
@@ -100,17 +105,17 @@ public class GPRMC extends GpsInfoRecord {
 
 
     public String toString() {
-		return fields[0]
-				+ " "
-				+ (isValid ? "Valid" : "Not Valid")
-				+ " at "
-				+ readingDate
-				+ (this.isValid ? (" Lat: " + Math.abs(latitude)
-						+ (latitude < 0 ? " S " : " N ") + " Long: "
-						+ Math.abs(longitude) + (longitude < 0 ? " W " : " E "))
-                        + " Speed: "+speed+"km/h "
-                        + " Direction: " + direction
-						: "");
-	}
+        return fields[0]
+                + " "
+                + (isValid ? "Valid" : "Not Valid")
+                + " at "
+                + readingDate
+                + (this.isValid ? (" Lat: " + Math.abs(latitude)
+                + (latitude < 0 ? " S " : " N ") + " Long: "
+                + Math.abs(longitude) + (longitude < 0 ? " W " : " E "))
+                + " Speed: " + speed + "km/h "
+                + " Direction: " + direction
+                : "");
+    }
 
 }
